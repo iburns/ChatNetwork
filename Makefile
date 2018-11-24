8397c79ec49cab7f14064392ed16740c315b8730
Ian Burns <ianburnsdev@gmail.com>
2018-11-24 17:36:45 -0500

    Server Rewrite to use Poll
    - Removed forking and threading in leu of using poll with an array of file descriptors
    - Handles multiple connected clients smoothly now

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 all: server client
 
 client: client.o
-	gcc -g -o client client.o
+	gcc -g -o client -lpthread client.o
 
 client.o: client.c
 	gcc -g -c client.c
 
 server: server.o
-	gcc -g -o server server.o
+	gcc -g -o server -lpthread server.o
 
 server.o: server.c
 	gcc -g -c server.c
--- server.c
+++ server.c
@@ -1,14 +1,28 @@
-// The server 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// The Server 
+//
+// For help with polling: https://www.ibm.com/support/knowledgecenter/ssw_ibm_i_71/rzab6/poll.htm
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/mman.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <pthread.h>
+#include <poll.h>
+#include <sys/ioctl.h>
+#include <errno.h>
 
 #define PORT 8080
 #define BUFLENGTH 1024
+
+#define MAX_CLIENTS 3
 
 struct client_data {
 	int sock;
@@ -32,6 +46,16 @@ void initializeServer(int *server_fd, struct sockaddr_in *address, int *opt) {
 		exit(EXIT_FAILURE);
 	}
 
+	int on = 1;
+	// Set socket to non blocking
+	int rc = ioctl(*server_fd, FIONBIO, (char *)&on);
+  	if (rc < 0)
+  	{
+  	  perror("ioctl() failed");
+  	  close(*server_fd);
+  	  exit(-1);
+  	}
+
 	address->sin_family = AF_INET;
 	address->sin_addr.s_addr = INADDR_ANY;
 	address->sin_port = htons(PORT);
@@ -41,23 +65,6 @@ void initializeServer(int *server_fd, struct sockaddr_in *address, int *opt) {
 		fprintf(stderr, "Bind failure.");
 		close(server_fd);
 		exit(EXIT_FAILURE);
-	}
-}
-
-void stopServer() {
-	printf("Stopping server...\n");
-	exit(EXIT_SUCCESS);
-}
-
-void addClientData(struct client_data *data) {
-	if (client_list == NULL) {
-		client_list = data;
-	} else {
-		struct client_data *last = client_list;
-		while (last->next != NULL) {
-			last = last->next;
-		}
-		last->next = data;
 	}
 }
 
@@ -77,36 +84,16 @@ void *handle_write(void *sock) {
 		inet_ntop(AF_INET, &(itr->address.sin_addr), ip, INET_ADDRSTRLEN);
 		printf("Connected clients: %s\n", ip);
 		while (itr->next) {
-			printf("test\n");
+			printf("Client:\n");
 			inet_ntop(AF_INET, &(itr->address.sin_addr), ip, INET_ADDRSTRLEN);
-			printf("Connected clients: %s\n", ip);
+			printf("\t%s\n", ip);
 			itr = itr->next;
 		}
 	}
 	printf("Stopping send thread...\n");
 
 	close(sock);
-	stopServer();
-}
 
-void *handle_read(void *data) {
-	struct client_data *ci = (struct client_data *)data;
-	char buffer[BUFLENGTH] = {0};
-	int valread;
-	char ip[INET_ADDRSTRLEN];
-	inet_ntop(AF_INET, &(ci->address.sin_addr), ip, INET_ADDRSTRLEN);
-	
-	while ((valread = read(ci->sock, buffer, 1024)) > 0) {
-		// Read
-		buffer[valread] = '\0';
-		printf("%s: %s", ip, buffer);
-	}
-
-	// Client disconnected
-	printf("%s has disconnected.\nStopping read thread...\n", ip);	
-
-	close(ci->sock);
-	stopServer();
 }
 
 /////////////////////////////////
@@ -115,11 +102,21 @@ void *handle_read(void *data) {
 int main(int argc, char const *argv[]) {
 	// Setup the variables
 	int server_fd, new_socket;
+	int rc, nfds = 1, timeout, current_size, compress_array;
 	struct sockaddr_in address;
 	int opt = 1;
 	int addrlen = sizeof(address);
+	int end_server = 0, close_conn = 0;
+	int i, j;	
+
+	char buffer[BUFLENGTH] = {0};
+
+	struct pollfd fds[200];
+
+	// Setup shared memory between processes
+	client_list = mmap(NULL, sizeof(client_list) * MAX_CLIENTS, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	
-	pthread_t readthread, writethread;	
+	pthread_t writethread;	
 
 	// Initialize
 	initializeServer(&server_fd, &address, &opt);
@@ -129,48 +126,131 @@ int main(int argc, char const *argv[]) {
 		fprintf(stderr, "Listen failure.");
 		exit(EXIT_FAILURE);
 	}
+
+	// Initialize fds space
+	memset(fds, 0, sizeof(fds));
+
+	// Setup listening socket
+	fds[0].fd = server_fd;
+	fds[0].events = POLLIN;	
+
+	// 3 minute timeout
+	timeout = (3 * 60 * 1000);
+
 	printf("Server started and listening...\n");
 
-	while(1) {	
-		// Accept new connections and assign to socket
-		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-			fprintf(stderr, "Accept failure.");
-			exit(EXIT_FAILURE);
+	do {
+		// Poll
+		rc = poll(fds, nfds, timeout);
+		
+		if (rc < 0) {
+			// Error
+			perror("poll() failed\n");
+			break;
 		}
+
+		if (rc == 0) {
+			printf("Timed out.\n");
+			break;
+		}
+
+		// One or more fds are available if we've reached this
+		current_size = nfds;
+		for (i = 0; i < current_size; i++) {
+			if (fds[i].revents == 0) {
+				continue;
+			}
+
+			if (fds[i].revents != POLLIN) {
+				printf("Error: revents = %d\n", fds[i].revents);
+				end_server = 1;
+				break;
+			}
+			
+			if (fds[i].fd == server_fd) {
+				// Listening socket is readable
+				// Accept all incoming connections
+				do {
+					if ((new_socket = accept(server_fd, NULL, NULL)) < 0) {
+						if (errno != EWOULDBLOCK) {
+							fprintf(stderr, "Accept failure.\n");
+							end_server = 1;
+						}
+						break;
+					}
 	
-		int pid = fork();
+					printf("New connection: %d\n", new_socket);
+					fds[nfds].fd = new_socket;
+					fds[nfds].events = POLLIN;
+					nfds += 1;
+				} while (new_socket != -1);
+			} else {
+				// This isn't the listening socket, so and existing connection must be readable
+				close_conn = 0;				
 
-		if (pid == 0) {
-			// is the child
-			close(server_fd);
-			
-			struct client_data *data;
-			data->sock = new_socket;
-			data->address = address;
-			data->next = NULL;
-			addClientData(data);
+				do {
+					rc = recv(fds[i].fd, buffer, sizeof(buffer), 0);
+					if (rc < 0) {
+						if (errno != EWOULDBLOCK) {
+							perror("recv fail\n");
+							close_conn = 1;
+						}
+						break;
+					}
 
-			
-			if (pthread_create(&writethread, NULL, handle_write, (void *)new_socket)) {
-				fprintf(stderr, "Write thread create error.\n");
-				exit(EXIT_FAILURE);
+					if (rc == 0) {
+						printf("Connection closed.\n");
+						close_conn = 1;
+						break;
+					}
+		
+					buffer[rc] = '\0';
+					printf("Recieved from %d: %s", fds[i].fd, buffer);	
+					rc = send(fds[i].fd, buffer, rc, 0);
+					if (rc < 0) {
+						perror("send error\n");
+						close_conn = 1;
+						break;
+					}
+				} while (1);
+
+				if (close_conn) {
+					close(fds[i].fd);
+					fds[i].fd = -1;
+					compress_array = 1;
+				}
 			}
-			
-			
-			if (pthread_create(&readthread, NULL, handle_read, (void *)data)) {
-				fprintf(stderr, "Read thread create error.\n");
-				exit(EXIT_FAILURE);	
-			}
-			
-			pthread_join(readthread, NULL);
-			//pthread_join(writethread, NULL);		
-			
-			exit(EXIT_SUCCESS);
-		} else {
-			// is server
-			close(new_socket);
 		}
-	}
+
+		// remove disconnected fds
+		if (compress_array)
+    		{
+      			compress_array = 0;
+      			for (i = 0; i < nfds; i++)
+      			{
+        			if (fds[i].fd == -1)
+        			{
+          				for(j = i; j < nfds; j++)
+          				{
+          				  fds[j].fd = fds[j+1].fd;
+          				}
+          				i--;
+          				nfds--;
+        			}
+      			}	
+		}
+
+	} while (end_server == 0);
+
+	printf("\n\nClosing server..\n\n");
+
+	// close all open sockets
+	for (i = 0; i < nfds; i++)
+  	{
+  		if(fds[i].fd >= 0)
+  			close(fds[i].fd);
+  	}	
+
 	return 0;
 }
 
